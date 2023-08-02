f3f0405bbc2fa0ba4270d7babae412ea70cf9782
Nathan Lisgo <nathan@lisgo.co.uk>
2023-08-02 22:22:07 +0100

    feat(tests): update test expectations to use toMatchObject #enhanced-preprints-biorxiv-xslt
    Changed assertion methods in app.test.ts and transform.test.ts from toStrictEqual to toMatchObject to allow for more flexible test cases.

--- app/transform.test.ts
+++ app/transform.test.ts
@@ -4,6 +4,6 @@ describe('transform', () => {
   it('should transform the XML string', async () => {
     const xml = '<message>Hello world!</message>';
     const transformedXml = transform(xml);
-    expect(await transformedXml).toStrictEqual({xml: '<message>Hello world!</message>'});
+    expect(await transformedXml).toMatchObject({xml: '<message>Hello world!</message>'});
   });
 });
